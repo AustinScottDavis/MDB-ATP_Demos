04000d762d1d299b4ac321cfdab4859c1f83d2a9
calzoom <calzoom@berkeley.edu>
2019-09-14 11:26:10 -0700

    info

--- Demo2/app/src/main/java/com/example/demo2/Adapter.java
+++ Demo2/app/src/main/java/com/example/demo2/Adapter.java
@@ -14,15 +14,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+/* Every recycler view needs an adapter (you can reuse adapters!)
+*
+* The purpose of an adapter is literally bind our app-data to the views that are displayed within a RecyclerView object, this class will tell our recycler how to populate the "sub-views", view in each row, with our data
+ */
 public class Adapter extends RecyclerView.Adapter<Adapter.CustomViewHolder> {
+
+    // It is helpful to have variable for context because `this` really only works when calling stuff from within Activities
     private Context context;
     List<Integer> data;
 
+    // Adapter construtor, whenever we make a new adapter from this class we need to pass in a context and the data that we want to bind
     Adapter(Context context, ArrayList<Integer> data) {
         this.context = context;
         this.data = data;
     }
 
+
+    // Pro-tip look-up @NonNull its a
     @NonNull
     @Override
     public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
